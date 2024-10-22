b0d7d8e0fbe1dc98a93a583992b1c16981801ad1
Sai <kldtks@live.com>
2024-10-22 19:27:22 +0800

    add log

--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -9,24 +9,14 @@ import (
 
 func Start() {
 
-	ticker := time.NewTicker(5 * time.Second)
+	interval := 5 * time.Second
+	ticker := time.NewTicker(interval)
+
 	defer ticker.Stop()
 
 	glog.Info("monitor tick 1")
-	go func() {
-		for {
-			glog.Info("monitor tick 2")
-			<-ticker.C
-			bfl.GetMyAppsInternal()
-		}
-	}()
-
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <-ticker.C:
-	// 			bfl.GetMyAppsInternal()
-	// 		}
-	// 	}
-	// }()
+	for range ticker.C {
+		glog.Info("monitor tick 2")
+		bfl.GetMyAppsInternal()
+	}
 }
