a641ddef67e55c177b18b768f77edfbc06c682fd
Sai <kldtks@live.com>
2025-08-26 19:29:54 +0800

    update API apps

--- internal/v2/appinfo/datawatcher_repo.go
+++ internal/v2/appinfo/datawatcher_repo.go
@@ -432,6 +432,7 @@ func (dwr *DataWatcherRepo) fetchAppInfoFromAPI(userID, sourceID, appName string
 			{
 				"appid":          appName,
 				"sourceDataName": sourceID,
+				"userid":         userID,
 			},
 		},
 	}
