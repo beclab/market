12e5737421949a61d6d6d14f4a1d584462980a15
Sai <kldtks@live.com>
2025-09-11 12:00:57 +0800

    remove old code

--- frontend/src/boot/smartEnginEntrance.js
+++
@@ -1,9 +0,0 @@
-import { boot } from 'quasar/wrappers';
-import BaseComponents from 'components/base';
-
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({ app }) => {
-	// something to do
-	app.use(BaseComponents);
-});
